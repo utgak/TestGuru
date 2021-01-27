c50e02bf540eb8d7a2bbfb9088ae9570ea5d18d3
utgak <degtyaryov.2002@mail.ru>
2021-01-27 14:24:54 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,11 +4,6 @@ class Test < ApplicationRecord
   has_many :question
 
   def self.get_titles_by_category(category)
-    arr = []
-    c_id = Category.where(title: category).order('title DESC').pluck(:id)
-    c_id.each do |id|
-      arr << Test.where(category_id: id).pluck(:title)
-    end
-    arr.flatten
+    Test.joins(:category).where(categories: {title: category}).order(:title).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
   has_many :test
   def get_passed_tests_by_level(level)
-    arr = []
-    PassedTest.where(test_id: Test.where(level: level), user_id: self.id).pluck(:test_id).each do | id |
-      arr << Test.find(id).title
-    end
-    arr
+    PassedTest.joins(:test, :user).where(users: {id: self.id}, tests: {level: level})
   end
 end
