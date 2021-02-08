7e7956fc065ccbf03d5a00a64e9f582a863879eb
utgak <degtyaryov.2002@mail.ru>
2021-02-08 20:39:06 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,9 @@
 class Test < ApplicationRecord
-  has_many :questions, dependent: :destroy
   belongs_to :user
   belongs_to :category
+  has_many :questions, dependent: :destroy
+  has_many :passed_tests, dependent: :destroy
+  has_many :users, through: :passed_tests, dependent: :destroy
 
   def self.titles_by_category(category)
     Test.joins("INNER JOIN 'categories' ON 'categories'.'id' = 'tests'.'category_id'").where(categories: {title: category}).order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :tests, dependent: :destroy
   has_many :passed_tests, dependent: :destroy
   has_many :tests, through: :passed_tests, dependent: :destroy
+
   def passed_tests_by_level(level)
     Test.joins("INNER JOIN 'passed_tests' ON 'passed_tests'.test_id = 'tests'.id").where(tests: {level: level}, passed_tests: {user_id: self.id})
   end
