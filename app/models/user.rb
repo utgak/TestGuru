bde7b24f321e3c7c542a270e0982bf028fbede02
utgak <degtyaryov.2002@mail.ru>
2021-02-10 02:07:01 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_many :tests, dependent: :destroy
+  has_many :created_tests, class_name: 'Test', dependent: :destroy
   has_many :passed_tests, dependent: :destroy
   has_many :tests, through: :passed_tests, dependent: :destroy
 
   def passed_tests_by_level(level)
-    Test.joins("INNER JOIN 'passed_tests' ON 'passed_tests'.test_id = 'tests'.id").where(tests: {level: level}, passed_tests: {user_id: self.id})
+    Test.joins(:passed_tests).where(tests: {level: level}, passed_tests: {user_id: self.id})
   end
 end
