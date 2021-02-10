3142a92c416ef60f307d3d4fb847db13a060ea47
utgak <72766681+utgak@users.noreply.github.com>
2021-02-10 19:03:08 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test', dependent: :destroy
   has_many :passed_tests, dependent: :destroy
-  has_many :tests, through: :passed_tests, dependent: :destroy
+  has_many :tests, through: :passed_tests
 
   def passed_tests_by_level(level)
     Test.joins(:passed_tests).where(tests: {level: level}, passed_tests: {user_id: self.id})
