e08b5e7300af3cc5c99c1435e8e011ad5eb0661f
utgak <degtyaryov.2002@mail.ru>
2021-02-19 20:30:05 +0300

    fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,9 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_test, only: %i[index create new]
+  before_action :find_test, only: %i[create new]
   before_action :find_question, only: %i[show edit update destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-
-  def index
-
-  end
 
   def show; end
 
@@ -36,7 +32,7 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    redirect_to test_questions_url(@question.test_id)
+    redirect_to test_url(@question.test_id)
   end
 
   private
