b889eddf3a6405b988f6f528bb94de0a1c6b3e7b
utgak <degtyaryov.2002@mail.ru>
2021-03-01 20:34:08 +0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(user)
-    flash[:notice] = "Hello, #{user.first_name}"
-    user.is_a?(Admin) ? admin_tests_path : root_path
+    user.is_a?(Admin) ? admin_tests_path : super
   end
 
   def configure_permitted_parameters
