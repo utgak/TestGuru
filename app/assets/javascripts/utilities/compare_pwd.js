6e73c2a01f3efd441fd95caf689cb8f9cd0bf1d2
utgak <degtyaryov.2002@mail.ru>
2021-03-07 13:18:22 +0300

    fix

--- app/assets/javascripts/utilities/compare_pwd.js
+++ app/assets/javascripts/utilities/compare_pwd.js
@@ -8,13 +8,15 @@ function comparePwd(){
     var pwd = document.querySelector('#user_password').value
     var pwd_confirm = document.querySelector('#user_password_confirmation').value
 
-    if (pwd_confirm.lenght > 0 ) {
+    if (pwd_confirm.length > 0) {
         if (pwd === pwd_confirm) {
-            this.querySelector('.octicon-check').classList.remove('hide')
-            this.querySelector('.octicon-x').classList.add('hide')
+            document.querySelector('.octicon-check').classList.remove('hide')
+            document.querySelector('.octicon-x').classList.add('hide')
         } else {
-            this.querySelector('.octicon-x').classList.remove('hide')
-            this.querySelector('.octicon-check').classList.add('hide')
+            document.querySelector('.octicon-x').classList.remove('hide')
+            document.querySelector('.octicon-check').classList.add('hide')
         }
+    } else {
+        document.querySelector('.octicon-x').classList.add('hide')
     }
 }
