0a482faef400237fbb5356da9c014867f33be593
utgak <degtyaryov.2002@mail.ru>
2021-03-09 22:36:25 +0300

    fix

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -4,20 +4,17 @@ class SortTests {
     }
 
     sortRowsByTitle() {
-        const table = document.querySelector('table')
-
-        // NodeList
-        // https://developer.mozilla.org/ru/docs/Web/API/NodeList
+        const table = this.querySelector('table')
         const sortedRows = Array.from(table.rows).slice(1)
 
-        if (document.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+        if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
             sortedRows.sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1)
-            document.querySelector('.octicon-arrow-up').classList.remove('hide')
-            document.querySelector('.octicon-arrow-down').classList.add('hide')
+            this.querySelector('.octicon-arrow-up').classList.remove('hide')
+            this.querySelector('.octicon-arrow-down').classList.add('hide')
         } else {
             sortedRows.sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? -1 : 1)
-            document.querySelector('.octicon-arrow-up').classList.add('hide')
-            document.querySelector('.octicon-arrow-down').classList.remove('hide')
+            this.querySelector('.octicon-arrow-up').classList.add('hide')
+            this.querySelector('.octicon-arrow-down').classList.remove('hide')
         }
         table.tBodies[0].append(...sortedRows)
     }
