dd62ec0a44600db9d2aa1e0d2b643639da39599c
utgak <degtyaryov.2002@mail.ru>
2021-03-09 22:22:07 +0300

    fix

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -1,47 +1,26 @@
 class SortTests {
     constructor(table) {
-        const control = table
-
-        control.addEventListener('click', this.sortRowsByTitle)
+        table.addEventListener('click', this.sortRowsByTitle.bind(this))
     }
-
-
 
     sortRowsByTitle() {
         const table = document.querySelector('table')
 
         // NodeList
         // https://developer.mozilla.org/ru/docs/Web/API/NodeList
-        const rows = table.querySelectorAll('tr')
-        const sortedRows = []
+        const sortedRows = Array.from(table.rows).slice(1)
 
-        // select all table rows except the first one which is the header
-        for (let i = 1; i < rows.length; i++) {
-            sortedRows.push(rows[i])
-        }
-
-        if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+        if (document.querySelector('.octicon-arrow-up').classList.contains('hide')) {
             sortedRows.sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1)
-            this.querySelector('.octicon-arrow-up').classList.remove('hide')
-            this.querySelector('.octicon-arrow-down').classList.add('hide')
+            document.querySelector('.octicon-arrow-up').classList.remove('hide')
+            document.querySelector('.octicon-arrow-down').classList.add('hide')
         } else {
             sortedRows.sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? -1 : 1)
-            this.querySelector('.octicon-arrow-up').classList.add('hide')
-            this.querySelector('.octicon-arrow-down').classList.remove('hide')
+            document.querySelector('.octicon-arrow-up').classList.add('hide')
+            document.querySelector('.octicon-arrow-down').classList.remove('hide')
         }
-
-        table.parentNode.replaceChild(SortTests.newTable(rows, sortedRows), table)
+        table.tBodies[0].append(...sortedRows)
     }
 
-    static newTable(rows, sortedRows) {
-        const sortedTable = document.createElement('table')
 
-        sortedTable.classList.add('table')
-        sortedTable.appendChild(rows[0])
-
-        for (let i = 0; i < sortedRows.length; i++) {
-            sortedTable.appendChild(sortedRows[i])
-        }
-        return sortedTable
-    }
 }
