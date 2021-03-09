2c9722080c035f61bec4b3dd938471dd62bbb150
utgak <degtyaryov.2002@mail.ru>
2021-03-09 04:14:36 +0300

    fix

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -1,6 +1,6 @@
 class SortTests {
     constructor() {
-        var control = document.querySelector('.sort-by-title')
+        const control = document.querySelector('.sort-by-title')
 
         control.addEventListener('click', this.sortRowsByTitle)
     }
