f30c45be7dc9c729b323e99c901880700e1397c4
utgak <degtyaryov.2002@mail.ru>
2021-03-12 02:26:47 +0300

    Revert "fix"
    This reverts commit 8416fd24

--- app/assets/javascripts/utilities/timer.js
+++ app/assets/javascripts/utilities/timer.js
@@ -1,6 +1,6 @@
 class Timer {
     constructor(elem, seconds) {
-        this.test_id = elem.dataset.test
+        this.test_passage_id = elem.dataset.test_passage_id
         this.elem = elem
         this.seconds = seconds
         this.id = setInterval(() => this.changeTime(), 1000)
@@ -9,7 +9,7 @@ class Timer {
     changeTime() {
         if (this.seconds <= 0){
             clearInterval(this.id)
-            window.location.href = `http://localhost:3000/test_passages/${this.test_id}/result`
+            window.location.href = `http://localhost:3000/test_passages/${this.test_passage_id}/result`
         } else {
             this.elem.innerHTML ="Time left: " + Math.floor(this.seconds / 60) + ":" + Math.floor(this.seconds % 60)
             this.seconds--
