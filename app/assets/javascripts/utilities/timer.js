8f3a4a0bb52b32f6d3867999ef37aa84757827ac
utgak <degtyaryov.2002@mail.ru>
2021-03-12 14:15:32 +0300

    commit

--- app/assets/javascripts/utilities/timer.js
+++ app/assets/javascripts/utilities/timer.js
@@ -1,6 +1,5 @@
 class Timer {
     constructor(elem, seconds) {
-        this.test_passage_id = elem.dataset.test_passage_id
         this.elem = elem
         this.seconds = seconds
     }
@@ -12,7 +11,7 @@ class Timer {
     changeTime() {
         if (this.seconds <= 0){
             clearInterval(this.id)
-            window.location.href = `http://localhost:3000/test_passages/${this.test_passage_id}/result`
+            document.getElementById('submit').click()
         } else {
             this.elem.innerHTML = Math.floor(this.seconds / 60) + ":" + Math.floor(this.seconds % 60)
             this.seconds--
